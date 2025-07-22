6796176f4a1dc85348a799afe37ad3a0808cf029
Anshu Sah <anshu7sah@gmail.com>
2025-07-22 21:07:13 +0545

    stripe payment code

--- router/order.js
+++ router/order.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { razorpay } from "../helper/razorpay_setup.js";
 import shortUUID from "short-uuid";
+import { stripe } from "../helper/stripe_setup.js";
 
 const router = express.Router();
 
@@ -20,6 +21,47 @@ router.post("/create-order", async (req, res) => {
   } catch (error) {
     console.log(error.message);
   }
+});
+
+// products.map(p=>{
+//   return {
+//     price_data:{
+//       currency:"inr",
+//       product_data:{
+//         name:p.name
+//       },
+//       unit_amount:p.price
+//     }
+//   }
+// })
+
+router.post("/create-checkout-session", async (req, res) => {
+  // const products=req.body;
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: "toy",
+              images: [
+                "https://img.freepik.com/premium-photo/toys-kids-play-time-colorful-fun-composition_594847-3791.jpg?semt=ais_hybrid&w=740",
+              ],
+            },
+            unit_amount: 10 * 100,
+          },
+          quantity: 10,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/failure",
+    });
+
+    return res.json({ url: session.id });
+  } catch (error) {}
 });
 
 export default router;
